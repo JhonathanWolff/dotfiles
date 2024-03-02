52bc137791f61b81da715b662cc40891902c5261
jhonathanwolff123@gmail.com <jhonathanwolff123@gmail.com>
2024-03-02 00:00:02 -0300

    config globais

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,9 @@
 vim.g.mapleader = " "
+vim.wo.relativenumber = true
 
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
 -- sudo apt-get install ripgrep
 -- sudo apt-get install python3-pip
@@ -12,14 +16,14 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -49,9 +53,6 @@ vim.keymap.set("n", "<leader>l", "$")
 vim.keymap.set("n", "p", "pgvy")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
-
-
-
 
 vim.keymap.set("n", "zq", ":wq<CR>")
 vim.keymap.set("n", "zs", ":w<CR>")
