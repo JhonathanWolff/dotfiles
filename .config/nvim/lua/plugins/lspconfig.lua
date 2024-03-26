cff632aa94ba669e6a279b68165bc3714c42e2b9
jhonathanwolff123@gmail.com <jhonathanwolff123@gmail.com>
2024-03-25 22:31:51 -0300

    new addons

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -27,7 +27,16 @@ return {
 			lspconfig.gopls.setup({capabilities = capabilities})
 			--setar novas  linguagens
 			lspconfig.lua_ls.setup({capabilities = capabilities})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'ç', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
 
 		end,
 	},
