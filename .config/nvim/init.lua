ad00a1556728217b5cdd9d76df5f81ffa73f7c04
DotFiles <dotfiles@myemail.com>
2024-04-21 22:16:19 -0300

    change cursor size

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set nohlsearch")
 
 vim.g.mapleader = " "
 vim.wo.relativenumber = true
+vim.cmd("set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175")
 
 vim.o.expandtab = true
 vim.o.tabstop = 4
